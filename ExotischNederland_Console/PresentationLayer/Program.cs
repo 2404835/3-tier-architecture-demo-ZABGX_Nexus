dfce3f756d147db18f47c51ee57d94c17771ab37
Roy Mengelers <roy.mengelers@gmail.com>
2024-12-29 19:53:39 +0100

    Create the user interface program, so the user is able to add inheemse soorten and show the ones that are registered in the session.

--- ExotischNederland_Console/PresentationLayer/Program.cs
+++ ExotischNederland_Console/PresentationLayer/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("This is now the presentation layer of the application :)");
+﻿var service = new InheemseSoortService();
+bool gebruikerMaaktValideKeuze = true;
+
+while (gebruikerMaaktValideKeuze)
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1. Registreer een nieuwe inheemse soort");
+    Console.WriteLine("2. Toon alle geregistreerde inheemse soorten");
+    Console.WriteLine("3. Afsluiten");
+    Console.Write("Maak een keuze: ");
+    string keuze = Console.ReadLine();
+  
+    if (keuze == "1")
+    {
+        Console.Write("Naam van de soort: ");
+        string naam = Console.ReadLine();
+
+        Console.Write("Naam van de locatie: ");
+        string locatieNaam  = Console.ReadLine();
+
+        Console.Write("Longitude: ");
+        string longitudeAntwoord = Console.ReadLine();
+        Decimal longitude = Convert.ToDecimal(longitudeAntwoord);
+
+        Console.Write("Latitude: ");
+        string latitudeAntwoord = Console.ReadLine();
+        Decimal laitude = Convert.ToDecimal(latitudeAntwoord);
+
+        Console.Write($"(Voorbeeld: 23-12-2024 14:45) {Environment.NewLine} Datum: ");
+        string datumAntwoord = Console.ReadLine();
+        DateTime datum = DateTime.ParseExact(datumAntwoord, "dd-MM-yyyy HH:mm",
+                                       System.Globalization.CultureInfo.InvariantCulture);;
+
+        service.RegistreerInheemseSoort(naam, locatieNaam, longitude, laitude, datum);
+        Console.WriteLine("Soort geregistreerd!\n");
+    }
+    else if (keuze == "2")
+    {
+        var soorten = service.HaalAlleInheemseSoortenOp();
+        Console.WriteLine("Geregistreerde soorten:");
+
+        foreach (var soort in soorten)
+        {
+            Console.WriteLine($"Naam: {soort.Naam}, Locatie: {soort.LocatieNaam}, Longitude: {soort.Longitude}, Latitude: {soort.Latitude}, Datum: {soort.Datum}");
+        }
+        Console.WriteLine();
+    }
+    else if (keuze == "3")
+    {
+        Console.WriteLine("Programma afgesloten.");
+        gebruikerMaaktValideKeuze = false;
+    }
+    else
+    {
+        Console.WriteLine("Ongeldige keuze, probeer opnieuw.\n");
+    }
+}
