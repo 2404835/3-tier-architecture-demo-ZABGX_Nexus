1587e36a91badee39601f3eda3cd0db2aac078ca
Roy Mengelers <roy.mengelers@gmail.com>
2024-12-29 19:30:36 +0100

    Add logice to the InheemseSoortService to add an inheemse soort and get all the registerd inheemse soorten

--- ExotischNederland_Console/BusinessLayer/InheemseSoortService.cs
+++ ExotischNederland_Console/BusinessLayer/InheemseSoortService.cs
@@ -6,4 +6,23 @@ internal class InheemseSoortService
     {
         _repository = new InheemseSoortRepository();
     }
+    
+    public void RegistreerInheemseSoort(string naam, string locatieNaam, DateTime datum, Decimal longitude, Decimal latitude)
+    {
+        var soort = new InheemseSoort
+        (
+            naam,
+            locatieNaam,
+            longitude,
+            latitude,
+            datum
+        );
+
+        _repository.VoegInheemseSoortToe(soort);
+    }
+
+    public List<InheemseSoort> HaalAlleInheemseSoortenOp()
+    {
+        return _repository.HaalAlleInheemseSoortenOp();
+    }
 }
