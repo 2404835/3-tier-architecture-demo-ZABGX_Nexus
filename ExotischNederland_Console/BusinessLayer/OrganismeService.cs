abeecd4a9e717f9d214526b8c803d0e85f53a338
Giel Vaes <2403703vaes@zuyd.nl>
2025-01-27 10:21:01 +0100

    de methodes aangepast zodat het werkt met de presentationLayer en met de classes

---
+++ ExotischNederland_Console/BusinessLayer/OrganismeService.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLTest
+{
+    internal class OrganismeService
+    {
+        private readonly OrganismeRepository _repository;
+
+        public OrganismeService()
+        {
+            _repository = new OrganismeRepository();
+        }
+
+        public void RegistreerDier(string naam, string oorsprong, string leefgebied)
+        {
+            var soort_dier = new Dier
+            (
+                naam,
+                oorsprong,
+                leefgebied
+            );
+
+            _repository.VoegDierToe(soort_dier);
+        }
+        public void RegistreerPlant(string naam, string oorsprong, decimal hoogte)
+        {
+            var soort_plant = new Plant
+            (
+                naam,
+                oorsprong,
+                hoogte
+            );
+
+            _repository.VoegPlantToe(soort_plant);
+        }
+
+        public List<Dier> HaalAlleDierenOp()
+        {
+            return _repository.HaalAlleDierenOp();
+        }
+        public List<Plant> HaalAllePlantenOp()
+        {
+            return _repository.HaalAllePlantenOp();
+        }
+
+    }
+}
+
